067d15925495e9770bc288e9a94ff55a62fd6341
Manan Patel <mananpatel@192-168-1-103.tpgi.com.au>
2020-07-24 21:42:21 +1000

    added some react and content

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
+// two modules required 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <h1>Hello World !! </h1>
-  ,
-  document.getElementById('root')
-);
+const flname = 'Manan Patel';
+const currDate = new Date().toLocaleDateString();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function tick() {
+  const element = (
+    <div>
+      <p> Date : {currDate} <br></br>Time : {new Date().toLocaleTimeString()}</p>
+    </div>
+  );
+  ReactDOM.render( 
+    <> 
+     <h1>{`Portfolio - ${flname}`}</h1> 
+      
+    <p>{element} </p>
+    
+     <p> Hardworking and motivated Software Engineer in the last semester of Bachelor’s degree (Computer and
+   Software systems) with a flair for creating elegant solutions in a limited time. Developed an attendance
+   management web app and QR-code based visitor management sys for a start-up. Passionate about software
+   architecture and cloud computing to shape the environment by contributing in developing world-leading
+   products in ranging domains.</p> 
+   <ol>
+       <h3>Professional Skills </h3>
+     <li>The longing to learn, perform various tasks and fulfil contending deadlines</li>
+     <li>A constructive and innovative approach to problem solving</li>
+     <li>Clear communication capabilities for efficient exchange of ideas and concepts</li>
+     <li>A solid cooperative person who likes to tune in, help other people and work together with colleagues</li>
+     <li>A versatile character who can adjust to change</li>
+     <li>Proven time-management skills developed from casual employment while studying and undertaking
+ internship and volunteering placements.</li>
+ 
+   </ol>
+   
+    </>,
+  
+    
+     document.getElementById('root'));
+  
+  }
+
+
+  setInterval(tick, 1000);
